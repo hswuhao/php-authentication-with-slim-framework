9a0845b3a368c9843644ddacdd3eff7ca4cb405e
klepra <klemen.pravdic@gmail.com>
2015-11-24 21:55:38 +0100

    not necessary logic commented out

--- app/start.php
+++ app/start.php
@@ -1,6 +1,8 @@
 <?php
 
 use Slim\Slim;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
 use Noodlehaus\Config;
 use app_namespace\User\User;
 session_cache_limiter(false);
@@ -11,7 +13,13 @@ ini_set('display_errors', 'On');
 define('INC_ROOT', dirname(__DIR__));
 require INC_ROOT.'/vendor/autoload.php';
 
-$app = new Slim(['mode'=>  file_get_contents(INC_ROOT.'/mode.php')]);
+$app = new Slim([
+    'mode'=>  file_get_contents(INC_ROOT.'/mode.php'),
+    'view' => new Twig(),
+    'templates.path' => INC_ROOT .'/app/views'
+       
+    
+    ]);
 
 $app->configureMode($app->config('mode'), function() use ($app){
     $app->config =  Config::load(INC_ROOT."/app/config/{$app->mode}.php");
@@ -23,5 +31,18 @@ require INC_ROOT."/app/database.php";
 $app->container->set('user', function (){
     return new User();
 });
-var_dump($app->user);
+
+//$app->get('/', function() use ($app){
+//    $app->render('home.php');
+//            
+//});
+$view=$app->view();
+
+$view->parserOptions = [
+    'debug' => $app->config->get('twig.debug')
+];
+$view->parserExtensions = [
+    new TwigExtension()
+];
+
 
