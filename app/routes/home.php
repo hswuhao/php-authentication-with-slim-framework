cc9de36ac14bd754479bd65f0923cc685d3faa8f
klepra <klemen.pravdic@gmail.com>
2015-12-08 20:41:58 +0100

    finished

--- app/routes/home.php
+++ app/routes/home.php
@@ -1,4 +1,10 @@
 <?php
 $app->get('/', function() use ($app){
     $app->render('home.twig');          
+})->name('home');
+
+
+$app->get('/flash', function() use ($app){
+    $app->flash('global', 'You have registered!');
+    $app->response->redirect($app->urlFor('home'));
 });
